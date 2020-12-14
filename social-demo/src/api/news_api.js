2fe7b577a5573fd7f56fd98896a0a22a4af8be42
CaelumVallis <CaelumVallis@users.noreply.github.com>
2020-12-14 17:01:03 +0200

    exposition

--- social-demo/src/api/news_api.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-	baseURL: "http://newsapi.org/v2/top-headlines?country=ua&",
-	params: { apiKey: "397d03946f5a4975822f605485d9c274" },
-});
--- social-demo/src/api/weather_api.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-	baseURL: "http://api.weatherapi.com/v1/current.json?key=b19f7bfd3c16409286c181513203011&q=Dnipropetrovsk",
-});
